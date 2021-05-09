2ddc91fd8b193caf1704bb199f2ec32c010745f9
Vladimir <q1zin@mail.ru>
2021-05-10 00:49:21 +0700

    added page thanks, minor bug fixes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-var btnMenu = 0;
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var btnMenu = 0;
+  var strech = document.getElementsByClassName("header__menu--strech");
 
-function closeMenu() {
-  document.querySelector(".header").classList.remove("header--active");
-  document.querySelector(".header__links-main").classList.remove("header__links-main--active");
-  document.querySelector(".header__links").classList.remove("header__links--active");
-  strech[1].style.width = 24 + "px";
-  strech[0].style.transform = "rotate(0deg)";
-  strech[2].style.transform = "rotate(0deg)";
-  strech[1].style.left = 0 + "px";
-  setTimeout(function () {
-    strech[0].style.top = 0 + "px";
-    strech[1].style.top = 8 + "px";
-    strech[2].style.top = 16 + "px";
-  }, 200)
-}
+  function closeMenu() {
+    document.querySelector(".header").classList.remove("header--active");
+    document.querySelector(".header__links-main").classList.remove("header__links-main--active");
+    document.querySelector(".header__links").classList.remove("header__links--active");
+    strech[1].style.width = 24 + "px";
+    strech[0].style.transform = "rotate(0deg)";
+    strech[2].style.transform = "rotate(0deg)";
+    strech[1].style.left = 0 + "px";
+    setTimeout(function () {
+      strech[0].style.top = 0 + "px";
+      strech[1].style.top = 8 + "px";
+      strech[2].style.top = 16 + "px";
+    }, 200)
+  }
 
-function openMenu() {
-  document.querySelector(".header").classList.add("header--active");
+  function openMenu() {
+    document.querySelector(".header").classList.add("header--active");
     document.querySelector(".header__links-main").classList.add("header__links-main--active");
     document.querySelector(".header__links").classList.add("header__links--active");
     strech[0].style.top = 8 + "px";
@@ -28,24 +30,25 @@ function openMenu() {
       strech[0].style.transformOrigin = "center";
       strech[2].style.transform = "rotate(-45deg)";
       strech[2].style.transformOrigin = "center";
-    }, 200)
-}
-
-document.querySelector(".header__menu").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  if (btnMenu) {
-    closeMenu();
-    btnMenu = 0;
-  } else {
-    openMenu();
-    btnMenu = 1;
+    }, 200);
   }
-})
 
-document.querySelector(".header__links").addEventListener("click", function (e) {
-  if (e.target.children.length == 0) {
-    closeMenu();
-    btnMenu = 0;
-  }
-})
+  document.querySelector(".header__menu").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (btnMenu) {
+      closeMenu();
+      btnMenu = 0;
+    } else {
+      openMenu();
+      btnMenu = 1;
+    }
+  })
+
+  document.querySelector(".header__links").addEventListener("click", function (e) {
+    if (e.target.children.length == 0) {
+      closeMenu();
+      btnMenu = 0;
+    }
+  })
+});
--- thanks.php
+++ thanks.php
@@ -5,19 +5,28 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Globalus - Главная страница</title>
+  <title>Globalus - Страница благодарности</title>
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/settings.css">
-  <link rel="stylesheet" href="css/swiper.css">
   <link rel="stylesheet" href="css/header.css">
-
+  <link rel="stylesheet" href="css/thanks.css">
 </head>
 
 <body>
+
   <?php include "header.php"; ?>
 
-  <script src="js/swiper.js"></script>
+  <section class="thanks">
+    <div class="thanks-wrap">
+      <h1 class="thanks__title">Спасибо!</h1>
+      <p class="thanks__text">В скором времени мы обязательно вам перезвоним и предложим наилудший тур</p>
+      <a href="index.php" class="thanks__btn">Вернуться на страницу</a>
+    </div>
+  </section>
+
+  <!-- Сдесь будет footer -->
+
   <script src="js/script.js"></script>
 </body>
 
